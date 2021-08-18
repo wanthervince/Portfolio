b46379a0aa42b0334d65ea18c25cd1a420ea3023
WanTher-VinCe <lonely.wolf.1594@gmail.com>
2021-08-18 00:31:10 -0500

    modificando nombre de proyecto en appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'portafolio-web';
+  title = 'Portfolio';
   firstOpen!: boolean;
   tactive!: boolean;
 
